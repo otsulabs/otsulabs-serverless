134f03a6aeb82ada85dba558bf3ce7b1b782712c
otsulabs <marketing@otsulabs.com>
2024-05-25 06:46:52 +0700

    replace missing environment variables

--- helpers/send-mail.js
+++ helpers/send-mail.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const { Resend } = require('resend');
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY || 're_ikqBigGK_39mYDJWGuYawNTYmsjRM3ZFH');
 
 async function sendMail(mailObject) {
   return await new Promise(async (resolve, reject) => {
