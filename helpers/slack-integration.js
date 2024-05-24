aa77aa5963346ce8939c6fd661b3aa466071c9fe
otsulabs <marketing@otsulabs.com>
2024-05-25 06:47:50 +0700

    replace missing environment variables

--- helpers/slack-integration.js
+++ helpers/slack-integration.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const { WebClient } = require("@slack/web-api");
-const bot = new WebClient(process.env.SLACK_TOKEN || 'xoxb-4821072139254-5684536683809-KKR4PbIOKSvGsjw64XbzSsnq');
+const bot = new WebClient(process.env.SLACK_TOKEN);
 
 async function slackPostMessage(data) {
   let type = data.type || 'contact'
@@ -9,7 +9,7 @@ async function slackPostMessage(data) {
     return await bot.files.uploadV2({
       file: data.url,
       filename: data.pathname,
-      channel_id: process.env.SLACK_CHANNEL || 'C04UNAMF2BZ',
+      channel_id: process.env.SLACK_CHANNEL,
       initial_comment: `
   ==============*New ${type}*=================
   
